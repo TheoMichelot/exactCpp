977269301b55b5446758a34caac622e2288a128b
TheoMichelot <theo.michelot@gmail.com>
2016-02-02 11:28:52 +0000

    document switchRate

--- switchRate.R
+++ switchRate.R
@@ -1,4 +1,13 @@
 
+#' Switching rates
+#' 
+#' Vector of switching rates when in state 'state' and in habitat 'habitat'.
+#' All zeros if state==habitat; otherwise, all zeros except for habitat
+#' (only possible to switch to state corresponding to current habitat).
+#' 
+#' @param state State
+#' @param habitat Habitat
+#' @param par Lambda parameters
 switchRate <- function(state,habitat,par,ns=3,nr=3)
 {
     rate <- array(0,c(ns,ns,nr))
