d7dd9a18e5a422e551edfce5f3d1807672f35a59
TheoMichelot <theo.michelot@gmail.com>
2016-02-09 17:24:29 +0000

    simplify parameters in rawMove

--- rawMove.R
+++ rawMove.R
@@ -4,25 +4,29 @@
 #' Returns mean and standard deviation of the animal's next locations, from the movement
 #' parameters and the previous location.
 #' 
-#' @param PAR1 PAR2 PAR3 Movement parameters
+#' @param par Vector of movement parameters (m, b, v)
 #' @param state State of underlying process
 #' @param deltaT Time interval between current and next location
-#' @param xx X-coordinate of current location
-#' @param yy Y-coordinate of current location
+#' @param x X-coordinate of current location
+#' @param y Y-coordinate of current location
+#' @param nbState Number of states
 #' 
-#' @details Assume for now exactly 3 parameter objects!
-#' Argumetns 'state', 'deltaT', 'xx', 'yy' may be scalars or vectors.
-rawMove <- function(PAR1,PAR2,PAR3,state,deltaT,xx,yy)
+#' @details Arguments 'state', 'deltaT', 'x', 'y' may be scalars or vectors.
+rawMove <- function(par,state,deltaT,x,y,nbState)
 {
-    #
-    # All states OU, with common centre
-    #
-    mux <- PAR1[1]
-    muy <- PAR1[2]
-    b <- -PAR2[state]
-    V <- PAR3[state]
+    # unpack the vector of parameters
+    m <- par[1:(2*nbState)]
+    b <- par[(2*nbState+1):(3*nbState)]
+    v <- par[(3*nbState+1):(4*nbState)]
+    
+    # select the right parameters
+    mux <- m[2*state-1]
+    muy <- m[2*state]
+    b <- -b[state]
+    v <- v[state]
+    
     phi <- exp(b*deltaT)
-    c1 <- 1-phi
-    SD <- sqrt(V*(1-phi^2))
-    return(list(emx=c1*(mux-xx),emy=c1*(muy-yy),sdx=SD,sdy=SD))
+    sd <- sqrt(v*(1-phi^2))
+    
+    return(list(emx=(1-phi)*(mux-x), emy=(1-phi)*(muy-y), sdx=sd, sdy=sd))
 }
