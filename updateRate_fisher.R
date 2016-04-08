faf46992ce7cc927e7b05118367a24d815c3095d
TheoMichelot <theo.michelot@gmail.com>
2016-04-08 17:03:49 +0100

    rates in intuitive order

--- updateRate_fisher.R
+++ updateRate_fisher.R
@@ -12,12 +12,12 @@ updateRate <- function (allData, indSwitch, kappa, shape1, shape2)
     counts <- minitabGeneric(allData[indSwitch-1,colState],allData[indSwitch,colState],
                              allData[indSwitch,colHabitat],ns=3,nr=3)
     
-    lambda21 <- kappa*rtruncbeta(1,shape1+counts[2,1,1],shape2+counts[2,2,1])
-    lambda31 <- kappa*rtruncbeta(1,shape1+counts[3,1,1],shape2+counts[3,3,1])
     lambda12 <- kappa*rtruncbeta(1,shape1+counts[1,2,2],shape2+counts[1,1,2])
-    lambda32 <- kappa*rtruncbeta(1,shape1+counts[3,2,2],shape2+counts[3,3,2])
     lambda13 <- kappa*rtruncbeta(1,shape1+counts[1,3,3],shape2+counts[1,1,3])
+    lambda21 <- kappa*rtruncbeta(1,shape1+counts[2,1,1],shape2+counts[2,2,1])
     lambda23 <- kappa*rtruncbeta(1,shape1+counts[2,3,3],shape2+counts[2,2,3])
+    lambda31 <- kappa*rtruncbeta(1,shape1+counts[3,1,1],shape2+counts[3,3,1])
+    lambda32 <- kappa*rtruncbeta(1,shape1+counts[3,2,2],shape2+counts[3,3,2])
     
     return(c(lambda12,lambda13,lambda21,lambda23,lambda31,lambda32))
 }
