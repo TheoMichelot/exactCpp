bdfa6b3468859aa7039f6fee92bc4c3217681a10
TheoMichelot <theo.michelot@gmail.com>
2016-08-19 13:56:33 +0100

    improve notations

--- minitabGeneric.r
+++ minitabGeneric.r
@@ -1,16 +1,16 @@
 
-#' Returns an array of dimensions (ns,ns,nr) filled with number of occurrences of each element in
+#' Returns an array of dimensions (nbState,nbState,nbHabitat) filled with number of occurrences of each element in
 #' (a1,a2,a3)
 #' 
 #' @param a1 Vector of values for 1st dimension
 #' @param a2 Vector of values for 2nd dimension
 #' @param a3 Vector of values for 3rd dimension
-#' @param ns Number of states (number of rows and columns of the array)
-#' @param nr Number of observations (number of layers of the array)
-minitabGeneric <- function(a1,a2,a3,ns,nr)
+#' @param nbState Number of states (number of rows and columns of the array)
+#' @param nbHabitat Number of habitats (number of layers of the array)
+minitabGeneric <- function(a1,a2,a3,nbState,nbHabitat)
 {
-    dims <- c(ns,ns,nr)
+    dims <- c(nbState,nbState,nbHabitat)
     pd <- prod(dims)
-    bin <- a1+ns*(a2-1)+ns*ns*(a3-1)
+    bin <- a1+nbState*(a2-1)+nbState*nbState*(a3-1)
     return(array(tabulate(bin, pd), dims))
 }
