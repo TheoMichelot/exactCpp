49e20ba850060de15707375a95773c5d9d0698a1
TheoMichelot <theo.michelot@gmail.com>
2016-08-19 14:01:57 +0100

    setupMCMC.R

---
+++ setupMCMC.R
@@ -0,0 +1,168 @@
+
+
+#' Setup data and parameters for MCMC algorithm
+#' 
+#' @param obs Matrix or data frame of observations, with columns x, y, and time.
+#' @param par0 List of initial values for the movement parameters. Must have three elements: mu, b, and v,
+#' each of length the number of states.
+#' @param rates0 Vector of initial values for the switching rates. Must be of length 
+#' number of states * (number of states - 1).
+#' @param homog List of mHomog, bHomog, and vHomog, which each indicate whether the corresponding movement
+#' parameter is homogeneous across states (TRUE) or not (FALSE -- default).
+#' @param priorMean List of means of priors for the movement parameters. Must have three elements: mu, b, and v,
+#' each of length the number of states.
+#' @param priorSD List of standard deviations of priors for the movement parameters. Must have three elements: 
+#' mu, b, and v, each of length the number of states.
+#' @param proposalSD List of standard deviations of proposals for the movement parameters. Must have three elements: 
+#' mu, b, and v, each of length the number of states.
+#' @param nbIter Number of iterations of the MCMC algorithm.
+#' @param map Map of habitats, if adaptative model.
+#' @param nbState Number of states.
+#' @param controls List of control parameters: 
+#' \item{kappa}{Kappa parameter from Blackwell et al (2015): upper bound for the switching rate out of any state;}
+#' \item{lenmin}{Minimum length of updated interval;}
+#' \item{lenmax}{Maximum length of updated interval;}
+#' \item{thin}{Thinning factor;}
+#' \item{prUpdateMove}{Probability of updating movement parameters at each iteration.}
+setupMCMC <- function(obs, par0, rates0, homog=list(mHomog=FALSE,bHomog=FALSE,vHomog=FALSE), 
+                      priorMean=NULL, priorSD=NULL, proposalSD=NULL, nbIter=5e5, map=NULL, nbState=NULL, 
+                      controls=list(kappa=2,lenmin=3,lenmax=6,thin=100,prUpdateMove=1))
+{
+    
+    # Are we working with the adaptative model?
+    if(is.null(map)) {
+        if(is.null(nbState))
+            stop("'nbState' needs to be specified if no map is given.")
+        adapt <- FALSE
+        
+        map <- matrix(1,nrow=1,ncol=1)
+    }
+
+    ###############
+    ## Read data ##
+    ###############
+    # observations
+    obs <- cbind(obs,NA,NA,NA,NA)
+    colnames(obs) <- c("X","Y","Time","State","Habitat","Jump","Behav")
+    nbObs <- nrow(obs)
+    
+    # enable references by "name" 
+    colX <- 1; colY <- 2; colTime <- 3; colState <- 4; colHabitat <- 5; colJump <- 6; colBehav <- 7
+
+    nbHabitat <- length(unique(c(map))) # count habitat types
+    if(adapt)
+        nbState <- nbHabitat
+    
+    # Set up rest of data matrix
+    obs[,colHabitat] <- findRegion(obs[,colX],obs[,colY],map)
+    obs[,colState] <- obs[,colHabitat]
+    obs[,colJump] <- 0 # jump for data point is always 0
+    obs[,colBehav] <- 0 # behavioural states not known
+    
+    #######################
+    ## Set up parameters ##
+    #######################
+    # Priors (on log scale for b and v)
+    if(is.null(priorMean))
+        priorMean <- n2w(par,nbState)
+    else
+        priorMean <- n2w(c(priorMean$mu,priorMean$b,priorMean$v),nbState)
+    
+    if(is.null(priorSD)) {
+        mPriorSD <- rep(c(10,10),nbState)
+        bPriorSD <- rep(10,nbState)
+        vPriorSD <- rep(10,nbState)
+        priorSD <- c(mPriorSD,bPriorSD,vPriorSD) 
+    } else
+        priorSD <- c(priorSD$mu,priorSD$b,priorSD$v) 
+
+    # MH proposals (on log scale for b and v)
+    if(is.null(proposalSD)) {
+        mProposalSD <- rep(c(0.03,0.03),nbState)
+        bProposalSD <- rep(0.03,nbState)
+        vProposalSD <- rep(0.03,nbState)
+        proposalSD <- c(mProposalSD,bProposalSD,vProposalSD)
+    } else
+        proposalSD <- c(proposalSD$mu,proposalSD$b,proposalSD$v)
+    
+    # for local update
+    SDP <- 0.15
+    
+    # Initial lambda (non-diagonal elements, filled row-wise)
+    if(is.null(rates0))
+        rates0 <- rep(1,nbState*(nbState-1))
+    
+    # prior beta parameters for lambdas
+    shape1 <- 4
+    shape2 <- 4
+
+    ####################
+    ## Prepare output ##
+    ####################
+    d <- format(Sys.time(), "%Y-%m-%d-%H%M")
+    
+    fileparams <- paste("params", d, ".txt", sep = "")
+    # Header
+    for(state in 1:nbState)
+        cat(file=fileparams, "mux", state, " muy", state, " ", append=TRUE, sep = "")
+    for(state in 1:nbState)
+        cat(file=fileparams, "b", state, " ", append=TRUE, sep = "")
+    for(state in 1:nbState)
+        cat(file=fileparams, "v", state, " ", append=TRUE, sep = "")
+    # First row
+    cat(file=fileparams, "\n", par, "\n", append=TRUE)
+    
+    filerates <- paste("rates", d, ".txt", sep = "")
+    # Header
+    for(state1 in 1:nbState)
+        for(state2 in 1:nbState)
+            if(state1!=state2)
+                cat(file=filerates, "lambda", state1, state2, " ", append=TRUE, sep = "")
+    # First row
+    cat(file=filerates, "\n", rates0, "\n", append = TRUE)
+    
+    ####################################
+    ## Prepare set of actual swtiches ##
+    ####################################
+    # indices of state switches
+    whichActual <- which(obs[-1,colState]!=obs[-nbObs,colState])+1
+    nbActual <- length(whichActual)
+    
+    dt <- 0.1*min(diff(obs[,colTime]))
+    
+    aSwitches <- cbind(X=obs[whichActual,colX],
+                       Y=obs[whichActual,colY],
+                       Time=obs[whichActual,colTime]-dt,
+                       State=obs[whichActual,colState],
+                       Habitat=obs[whichActual,colHabitat],
+                       Jump=rep(1,nbActual),
+                       Behav=rep(0,nbActual))
+    
+    # Controls
+    if(is.null(controls$lenmin))
+        controls$lenmin <- 3
+    if(is.null(controls$lenmqx))
+        controls$lenmax <- 6
+    if(is.null(controls$thin))
+        controls$thin <- 100
+    if(is.null(controls$prUpdateMove))
+        controls$prUpdateMove <- 1
+    
+    return(list(obs=obs,
+                par0=c(par0$mu,par0$b,par0$v),
+                rates0=rates0,
+                nbState=nbState,
+                priorMean=priorMean,
+                priorSD=priorSD,
+                proposalSD=proposalSD,
+                controls=controls,
+                nbIter=nbIter,
+                map=map,
+                homog=homog,
+                fileparams=fileparams,
+                filerates=filerates,
+                aSwitches=aSwitches,
+                nbActual=nbActual,
+                whichActual=whichActual,
+                adapt=adapt))
+}
