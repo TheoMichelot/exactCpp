c37e0aebbc86901847e8f5afa96b90095a446a82
TheoMichelot <theo.michelot@gmail.com>
2016-08-22 12:09:07 +0100

    improve simulation experiment example

--- example.R
+++ example.R
@@ -14,31 +14,39 @@ sourceCpp("moveLike.cpp")
 sourceCpp("updatePar.cpp")
 sourceCpp("localUpdate.cpp")
 
+source("simDataOU.R")
 source("setupMCMC.R")
 source("MCMCloop.R")
-
-# load observations
-obs <- as.matrix(read.csv("~/Grive/software/simulation/obs.csv"))
 
 # define map
 map <- matrix(1,nrow=12,ncol=12)
 map[7:12,] <- 2
 
-nbState <- 2
+# movement parameters
+mu <- matrix(c(5,6,5,6), ncol=2, byrow=TRUE)
+b <- c(-2,-0.5)
+v <- c(3,0.5)
+
+rates <- rep(0.5,2)
+
+set.seed(1)
+
+# simulate observations
+sim <- simDataOU(mu=mu, b=b, v=v, rates=rates, map=map)
+obs <- as.matrix(sim[,c(1,2,4)])
+
+# initial parameters
+nbState <- length(b)
 mpar <- rep(c(0,0),nbState) # centers of attraction
 bpar <- rep(1,nbState) # coefficients for matrix B
 vpar <- rep(2,nbState) # coefficients for matrix Lambda
 par0 <- list(m=mpar,b=bpar,v=vpar)
 rates0 <- rep(1,nbState*(nbState-1))
 
-homog=list(mHomog=FALSE, bHomog=TRUE, vHomog=TRUE)
+homog=list(mHomog=TRUE, bHomog=FALSE, vHomog=FALSE)
 
-set.seed(1)
 allArgs <- setupMCMC(obs, par0, rates0, map=map, homog=homog)
 mod <- MCMCloop(allArgs)
 
-allPlots(nbState=2, fileparams=mod$fileparams, filerates=mod$filerates, 
-         truePar=c(3,6,9,6, # mu
-                   -2,-2, # b
-                   2,2) # v
-)
+allPlots(nbState=nbState, fileparams=mod$fileparams, filerates=mod$filerates,
+         truePar=c(as.vector(t(mu)),b,v))
